71ace93f341519e8e8eb7c32023447c70e224395
Hamid Feizabadi <hamidfzm@gmail.com>
2026-05-12 16:42:31 +0000

    chore: update glyph linux formula to 0.6.0

--- Formula/glyph.rb
+++ Formula/glyph.rb
@@ -2,16 +2,16 @@ class Glyph < Formula
   desc "Cross-platform markdown viewer"
   homepage "https://github.com/hamidfzm/glyph"
   license "MIT"
-  version "0.5.0"
+  version "0.6.0"
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/hamidfzm/glyph/releases/download/v0.5.0/Glyph_0.5.0_amd64.deb"
-      sha256 "3feb1579f7f9863ff8a16b7295a4260d3664716baad9618f672b2e712c8625d3"
+      url "https://github.com/hamidfzm/glyph/releases/download/v0.6.0/Glyph_0.6.0_amd64.deb"
+      sha256 "8f2e4e17dbc5393003f38e629ffcf6fa3460d47739eccfe402dce02ccc805653"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/hamidfzm/glyph/releases/download/v0.5.0/Glyph_0.5.0_arm64.deb"
-      sha256 "a2798988ed58d49436e0036aa82cfee45abd3f9274013108a68d139e0010652d"
+      url "https://github.com/hamidfzm/glyph/releases/download/v0.6.0/Glyph_0.6.0_arm64.deb"
+      sha256 "d53fdd1ba047a0c728863b87a5b995b84ba22a0fd98dc5555d194f907f92932d"
     end
   end
 
