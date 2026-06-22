2a69f8454664cac7f6cf6221938343fccbbd98c3
Hamid Feizabadi <hamidfzm@gmail.com>
2026-06-22 15:11:40 +0000

    chore: update glyph to 0.12.0

--- Casks/glyph.rb
+++ Casks/glyph.rb
@@ -1,6 +1,6 @@
 cask "glyph" do
-  version "0.11.0"
-  sha256 "9d05354173e59f9bc6ee421628c209d7b32309d0ac166d9a04e694498f0564eb"
+  version "0.12.0"
+  sha256 "c86e887842513f90737ea83154ef30b5963ae65dd0d005ac6009fccf3dc2539f"
 
   url "https://github.com/hamidfzm/glyph/releases/download/v#{version}/Glyph_#{version}_universal.dmg"
   name "Glyph"
@@ -13,6 +13,7 @@ cask "glyph" do
   end
 
   app "Glyph.app"
+  binary "#{appdir}/Glyph.app/Contents/MacOS/Glyph", target: "glyph"
 
   postflight do
     system_command "/usr/bin/xattr",
